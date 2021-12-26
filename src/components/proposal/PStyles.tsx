1e651139e08a60608fd9a4dc22fb360b79cf1735
Beksultan <70627749+beksultan-07@users.noreply.github.com>
2021-12-26 17:19:05 +0600

    add proposal

---
+++ src/components/proposal/PStyles.tsx
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+export const ProposalWrap = styled.div`
+    padding-bottom: 20px; 
+`
+
+export const ProposalList = styled.ul`
+    list-style: none;
+    padding: 0;
+    display: grid;
+    gap: 10px;
+    grid-template-columns: repeat(4, 1fr);
+
+    a{
+        text-decoration: none;
+    }
+
+    @media (max-width: 768px) {
+        grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 425px) {
+        grid-template-columns: repeat(1, 1fr);
+        text-align:center;
+  }
+`
+
+export const PropocalItem = styled.li`
+    flex: 1;
+    padding: 0 10px;
+    display: flex;
+    height: 100%;
+    flex-direction: column;
+    justify-content: flex-start;
+    flex-wrap: wrap;
+    border-radius: 10px;
+    box-shadow: 0px 10px 20px rgb(0 0 0 / 6%);
+    @media (max-width: 425px) {
+        padding-top: 20px;
+    }
+    &:hover{
+        background: #E5E5E5;
+        transition: .3s;
+    } 
+`
+
+export const PropocalitemImg = styled.img`
+    width: 70px;
+    margin: 0 auto;
+`
+
+export const PropocalitemTitle = styled.h3`
+  font-size: 16px;
+  font-style: normal;
+font-weight: 500;
+  margin: 24px 0 10px;
+  color: #0f2b46;
+  
+`
+
+export const PropocalitemText = styled.p`
+  font-size: 14px;
+  margin: 0;
+color: #0f2b46;
+`
---
+++ src/components/proposal/Proposal.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { PropocalItem, PropocalitemImg, PropocalitemText, PropocalitemTitle, ProposalList, ProposalWrap } from './PStyles'
+import icon1 from '../../assets/images/fiat-light.png'
+import icon2 from '../../assets/images/low-fees-light.png'
+import icon3 from '../../assets/images/security-light.png'
+import icon4 from '../../assets/images/spot-dark.png'
+import { Link } from 'react-router-dom'
+
+
+
+const Proposal:React.FC = () => {
+    const [info, setinfo] = React.useState([
+        {
+            title: 'Торговля стала проще',
+            text: 'Покупайте, продавайте, управляйте и торгуйте более 150 криптовалютами (включая Bitcoin, BNB и Ethereum). Увеличьте свою прибыль с Envoys Vision',
+            img: icon4
+        },
+        {
+            title: 'Удобный процесс торговли',
+            text: 'Наши комиссии и привлекательная VIP-программа превзошли всех конкурентов. Наслаждайтесь одними из самых низких комиссий за транзакции в криптовалюте.',
+            img: icon2
+        },
+        {
+            title: 'Высочайшие стандарты безопасности',
+            text: 'Наши комплексные меры безопасности защищают ваши данные и активы от всех рисков.',
+            img: icon3
+        },
+        {
+            title: 'Минимальные суммы для покупки',
+            text: 'С Envoys Vision вы можете купить Bitcoin или любую другую криптовалюту на сумму от 15 долларов. Используйте свою дебетовую или кредитную карту. Начните торговлю уже сегодня.',
+            img: icon1
+        },
+    ])
+
+    return (
+        <ProposalWrap>
+            <ProposalList>
+                {info.map((item, index) => {
+                    return (
+                        <Link key={index} to='#'>
+                            <PropocalItem>
+                                <PropocalitemImg src={item.img}/>
+                                <PropocalitemTitle>{item.title}</PropocalitemTitle>
+                                <PropocalitemText>{item.text}</PropocalitemText>
+                            </PropocalItem>
+                        </Link>
+                    )
+                })}
+            </ProposalList>
+        </ProposalWrap>
+    )
+}
+
+export default Proposal
